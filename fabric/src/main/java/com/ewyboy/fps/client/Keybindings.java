73385beaa7f97672917c7607481b13e899cb657b
eivindnorling <eivind.norling@tidsbanken.no>
2022-04-04 19:11:54 +0200

    v1.2.0

--- fabric/src/main/java/com/ewyboy/fps/client/Keybindings.java
+++ fabric/src/main/java/com/ewyboy/fps/client/Keybindings.java
@@ -7,6 +7,7 @@ import com.ewyboy.fps.util.Translation;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.KeyMapping;
 import org.lwjgl.glfw.GLFW;
 
@@ -18,8 +19,10 @@ public class Keybindings {
     private static KeyMapping all;
 
     public static void setup() {
-        initKeyBinding();
-        clickEvent();
+        if (FabricLoader.getInstance().isModLoaded("fabric")) {
+            initKeyBinding();
+            clickEvent();
+        }
     }
 
     private static void initKeyBinding() {
@@ -39,22 +42,21 @@ public class Keybindings {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             Settings clientSettings = SettingsLoader.CONFIG;
 
-            while (fps.consumeClick()) clientSettings.toggleFps = !clientSettings.toggleFps;
-            while (ping.consumeClick()) clientSettings.togglePing = !clientSettings.togglePing;
-            while (memory.consumeClick()) clientSettings.toggleMemory = !clientSettings.toggleMemory;
+            while (fps.consumeClick()) clientSettings.toggleFps = ! clientSettings.toggleFps;
+            while (ping.consumeClick()) clientSettings.togglePing = ! clientSettings.togglePing;
+            while (memory.consumeClick()) clientSettings.toggleMemory = ! clientSettings.toggleMemory;
 
-            if(all.consumeClick()) {
-                toggleAll = !toggleAll;
+            if (all.consumeClick()) {
+                toggleAll = ! toggleAll;
 
-                if (clientSettings.toggleFps != toggleAll) clientSettings.toggleFps = !clientSettings.toggleFps;
-                if (clientSettings.togglePing != toggleAll) clientSettings.togglePing = !clientSettings.togglePing;
-                if (clientSettings.toggleMemory != toggleAll) clientSettings.toggleMemory = !clientSettings.toggleMemory;
+                if (clientSettings.toggleFps != toggleAll) clientSettings.toggleFps = ! clientSettings.toggleFps;
+                if (clientSettings.togglePing != toggleAll) clientSettings.togglePing = ! clientSettings.togglePing;
+                if (clientSettings.toggleMemory != toggleAll) clientSettings.toggleMemory = ! clientSettings.toggleMemory;
 
-                clientSettings.toggleFps = !clientSettings.toggleFps;
-                clientSettings.togglePing = !clientSettings.togglePing;
-                clientSettings.toggleMemory = !clientSettings.toggleMemory;
+                clientSettings.toggleFps = ! clientSettings.toggleFps;
+                clientSettings.togglePing = ! clientSettings.togglePing;
+                clientSettings.toggleMemory = ! clientSettings.toggleMemory;
             }
-
         });
     }
 
